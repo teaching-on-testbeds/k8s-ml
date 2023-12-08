db87a3d11960cd4119351c76a73c177d81ca4540
Fraida Fund <ffund@nyu.edu>
2023-12-07 21:57:15 -0500

    Add cloudlab profile

---
+++ profile.py
@@ -0,0 +1,60 @@
+"""Bring up three VMs and set up a Kubernetes cluster.
+
+Instructions:
+Wait for the profile instance to start, and then wait for the startup scripts 
+to finish running (resources turn green, with a check mark in the corner).
+This may take about 30 minutes.
+
+Then, open a shell and log in to the controller.
+To verify the Kubernetes and Docker install, run
+
+```
+kubectl get nodes
+```
+
+and
+
+```
+docker run hello-world
+```
+
+See https://github.com/teaching-on-testbeds/k8s-ml/blob/main/README.md
+for further instructions.
+"""
+
+import geni.portal as portal
+import geni.rspec.pg as rspec
+
+# Create a Request object to start building the RSpec.
+request = portal.context.makeRequestRSpec()
+ 
+node_0 = request.XenVM("node-0")
+node_0.cores = 4
+node_0.ram = 8192
+node_0.exclusive = True
+node_0.routable_control_ip = True
+node_0.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD'
+iface0 = node_0.addInterface('interface-0', rspec.IPv4Address('192.168.1.10','255.255.255.0'))
+node_0.addService(pg.Execute('/bin/sh','bash /local/repository/reserve_cloudlab/setup.sh'))
+
+node_1 = request.XenVM("node-1")
+node_1.cores = 4
+node_1.ram = 8192
+node_1.exclusive = True
+node_1.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD'
+iface1 = node_1.addInterface('interface-1', rspec.IPv4Address('192.168.1.11','255.255.255.0'))
+
+node_2 = request.XenVM("node-2")
+node_2.cores = 4
+node_2.ram = 8192
+node_2.exclusive = True
+node_2.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD'
+iface2 = node_2.addInterface('interface-2', rspec.IPv4Address('192.168.1.12','255.255.255.0'))
+
+link_0 = request.Link('net0')
+link_0.addInterface(iface1)
+link_0.addInterface(iface0)
+link_0.addInterface(iface2)
+
+# Print the RSpec to the enclosing page.
+portal.context.printRequestRSpec()
---
+++ reserve_cloudlab/setup.sh
@@ -0,0 +1,61 @@
+sudo apt update
+sudo apt -y install virtualenv
+
+# get Kubespray + materials for this assignment
+git clone --branch release-2.22 https://github.com/kubernetes-sigs/kubespray.git
+git clone https://github.com/teaching-on-testbeds/k8s-ml.git
+
+mv kubespray/inventory/sample kubespray/inventory/mycluster
+cp k8s-ml/config/k8s-cluster.yml kubespray/inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
+cp k8s-ml/config/inventory.py    kubespray/contrib/inventory_builder/inventory.py
+cp k8s-ml/config/addons.yml      kubespray/inventory/mycluster/group_vars/k8s_cluster/addons.yml
+
+# wait for other hosts to come up
+until sudo ssh -o StrictHostKeyChecking=no node-1 true >/dev/null 2>&1; do echo "Waiting for node-1 to come up"; sleep 5; done
+until sudo ssh -o StrictHostKeyChecking=no node-2 true >/dev/null 2>&1; do echo "Waiting for node-2 to come up"; sleep 5; done
+
+# install keys at all other hosts
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N ""
+PUBKEY=$(cat /users/$USER/.ssh/id_rsa.pub)
+echo $PUBKEY >> /users/$USER/.ssh/authorized_keys
+sudo ssh -o StrictHostKeyChecking=no node-1 "echo $PUBKEY >> /users/$USER/.ssh/authorized_keys"
+sudo ssh -o StrictHostKeyChecking=no node-2 "echo $PUBKEY >> /users/$USER/.ssh/authorized_keys"
+
+virtualenv -p python3 myenv
+source myenv/bin/activate
+cd kubespray
+pip3 install -r requirements.txt
+
+declare -a IPS=(192.168.1.10 192.168.1.11 192.168.1.12)
+CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
+
+ansible-playbook -i inventory/mycluster/hosts.yaml  --become --become-user=root cluster.yml
+
+# allow kubectl access for non-root user
+for login_user in $(ls /users)
+do 
+    sudo cp -R /root/.kube /users/$login_user/.kube
+    sudo chown -R $login_user /users/$login_user/.kube
+done
+
+# Add the user to the docker group on all hosts
+sudo groupadd -f docker
+ssh -o StrictHostKeyChecking=no node-1 "sudo groupadd -f docker"
+ssh -o StrictHostKeyChecking=no node-2 "sudo groupadd -f docker"
+for login_user in $(ls /users)
+do 
+    sudo usermod -aG docker $login_user
+    ssh -o StrictHostKeyChecking=no node-1 "sudo groupadd -f docker; sudo usermod -aG docker $login_user"
+    ssh -o StrictHostKeyChecking=no node-2 "sudo groupadd -f docker; sudo usermod -aG docker $login_user"
+
+done
+
+
+# Set up private distribution registry
+# needs to be in new session
+ssh -o StrictHostKeyChecking=no node-1 "docker run -d -p 5000:5000 --restart always --name registry registry:2"
+
+sudo wget https://raw.githubusercontent.com/teaching-on-testbeds/k8s-ml/main/config/daemon.json -O /etc/docker/daemon.json; sudo service docker restart
+ssh -o StrictHostKeyChecking=no node-1 "sudo wget https://raw.githubusercontent.com/teaching-on-testbeds/k8s-ml/main/config/daemon.json -O /etc/docker/daemon.json; sudo service docker restart"
+ssh -o StrictHostKeyChecking=no node-2 "sudo wget https://raw.githubusercontent.com/teaching-on-testbeds/k8s-ml/main/config/daemon.json -O /etc/docker/daemon.json; sudo service docker restart"
+
